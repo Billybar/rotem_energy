3f0ef95c583f3ab968d5bf6b204f209690f50a7d
Billybar <barchaim10@gmail.com>
2020-11-06 11:33:40 +0200

    final change DB_PWD

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PWD', '6tfc5RDX');
+define('DB_USER', 'billybar');
+define('DB_PWD', '6tfc%RDX');
 define('DB_DATABASE', 'rotem_energy');
 
 $link = mysqli_connect(DB_SERVER, DB_USER, DB_PWD, DB_DATABASE);
